33373ee5b151cfd7e5eb533e3f7ceae1855aece3
Shaik Karimulla <karimulla.s@prodapt.com>
2023-04-03 13:08:14 +0530

    git repository removed

--- src/dialogs/About.vue
+++ src/dialogs/About.vue
@@ -4,12 +4,6 @@
       <v-card-title>{{ item.title }}</v-card-title>
       <v-card-subtitle>{{ item.subtitle }}</v-card-subtitle>
     </div>
-    <v-card-title
-      >Github Repository -
-      <a href="https://github.com/karim732/shaikkarim" target="_blank">
-        Link</a
-      ></v-card-title
-    >
     <v-card-actions class="d-flex justify-end">
       <v-btn color="red" @click="$emit('ChildClose')" class="text-btn"
         >Close</v-btn
@@ -26,7 +20,7 @@ export default {
         { title: "VueJs 3", subtitle: "Options API, JavaScript, Vue router." },
         {
           title: "Vuetify 3",
-          subtitle: "Responsive, MDI - icons.",
+          subtitle: "Responsive, MDI - Icons.",
         },
         { title: "Database", subtitle: "Firebase" },
       ],
